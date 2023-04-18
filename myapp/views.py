c213ff6a6176d7a94d7fce1c3ceaf4a786305d05
AnthonyAlagna <alalagna@uwm.edu>
2023-04-18 12:30:58 -0500

    define login page view, test_login, remove redundant test_models

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
 # Create your views here.
+
+
+class login(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = User.objects.filter(
+            User_LogName=username, User_LogPass=password)
+        if user:
+            return redirect('index')
+        else:
+            return redirect('login')
