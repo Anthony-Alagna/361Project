765d484f57bc76b0c280063fe0e0e6272b2ed269
aburmai2 <aburmai2@uwm.edu>
2023-04-18 11:56:21 -0500

    added classes under the myApp section so that we can create and begin unit tests

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,8 @@
+import unittest
+
 from django.test import TestCase
+from models import User, Section, Course, courseToUser
+# we need to import the actual classes from my app
+
 
 # Create your tests here.
