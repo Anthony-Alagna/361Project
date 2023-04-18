ac259263cb125e1486daea1834b56ea06a615715
Brandon <bbuckner49@hotmail.com>
2023-04-18 13:20:33 -0500

    Worked on create test for edit account info

--- tests/test_account_personal_information.py
+++ tests/test_account_personal_information.py
@@ -1,15 +1,25 @@
 import unittest
 from django.test import TestCase, Client
 from myapp.models import User
+from unittest.mock import Mock
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.email = "test@gmail.com"
-        self.username = "testUsername"
-
+        user = Mock()
+        user.email = "test@gmail.com"
+        user.email = "test@gmail.com"
+        user.username = "testUsername"
+        user.phone = "1111111111"
+        user.address = "123 Blvd"
+        user.fName = "Bob"
+        user.lName = "Smith"
     def test_get_account_info(self):
         self.assertEqual("test@gmail.com", self.getAccountInfo(self.username))
+
+    def test_edit_account_info(self):
+        self.setUp
+        self.assertEqual(user.email, "test@gmail.com")
 
 
 if __name__ == '__main__':
