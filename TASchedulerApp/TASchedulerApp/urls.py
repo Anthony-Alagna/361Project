cddf3fb373ae2078a99e45174ea6a3e348b81e81
AnthonyAlagna <alalagna@uwm.edu>
2023-04-18 12:59:32 -0500

    add login to urls, inital implementation of views

--- TASchedulerApp/TASchedulerApp/urls.py
+++ TASchedulerApp/TASchedulerApp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import Login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Login.as_view(), name='login'),
 ]
