626171b7f597b63231836c07be9007e44d638a65
matthew <adasmatt@gmail.com>
2023-04-18 12:39:48 -0500

    Added to courses.py

---
+++ myapp/Classes/courses.py
@@ -0,0 +1,7 @@
+class Course:
+    def __init__(self, courseID, name, code, instructor, ta):
+        self.courseID = courseID
+        self.name = name
+        self.code = code
+        self.instructor = instructor
+        self.ta = ta
