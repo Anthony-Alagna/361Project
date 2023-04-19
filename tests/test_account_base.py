3dcf08648b7c2f4cb5a5ac3d9d28156999aa16b8
kmaule15 <kaylamaule15@gmail.com>
2023-04-19 17:06:56 -0500

    created tests for account base page

--- tests/test_account_base.py
+++ tests/test_account_base.py
@@ -3,6 +3,7 @@ from myapp.models import User
 
 
 # unit tests for functionality on account base page (searching for user, etc.)
+# using User as class for methods until Account class is added to project
 
 # Method: Account.searchUser(username: str)
 class TestSearchUser(TestCase):
@@ -41,24 +42,71 @@ class TestSearchUser(TestCase):
         result = User.searchUser(username)
         self.assertEqual(result, self.user1, "searchUser should have returned the username that matches the parameter")
 
-# methods I'll be creating tests for
 
-# Account.getAccountInfo (username:str)
+# Account.filterUser(usertype: str)
+class TestFilterUser(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(User_ID='1', User_Name='Cricket', User_Email='user1@example.com',
+                                         User_Type='Student', User_Phone='1234567890', User_Address='123 Main St',
+                                         User_LogName='user1', User_LogPass='password', User_isGrader='no',
+                                         User_begin='2022-01-01 00:00:00', User_Updated='2023-04-18 00:00:00')
+        self.user2 = User.objects.create(User_ID='2', User_Name='Noodle', User_Email='user2@example.com',
+                                         User_Type='Student', User_Phone='0987654321', User_Address='456 Elm St',
+                                         User_LogName='user2', User_LogPass='password1', User_isGrader='no',
+                                         User_begin='2022-01-01 00:00:00', User_Updated='2023-04-17 00:00:00')
+        self.user3 = User.objects.create(User_ID='3', User_Name='Toby', User_Email='user3@example.com',
+                                         User_Type='Professor', User_Phone='0987654322', User_Address='457 Elm St',
+                                         User_LogName='user3', User_LogPass='password2', User_isGrader='no',
+                                         User_begin='2022-01-01 00:00:00', User_Updated='2023-04-17 00:00:00')
 
-# Account.getUser()
+    def test_filter_user(self):
+        usertype = "Student"
+        result = User.filterUser(usertype)
+        self.assertEqual(result, [self.user1, self.user2],
+                         "filterUser didn't return all of the users matching the type parameter")
 
 
-# Account.getAccountInfo (username:str)
+# Account.deleteUser(username: str)
+class TestDeleteUser(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create(User_ID='1', User_Name='Cricket', User_Email='user1@example.com',
+                                         User_Type='student', User_Phone='1234567890', User_Address='123 Main St',
+                                         User_LogName='user1', User_LogPass='password', User_isGrader='no',
+                                         User_begin='2022-01-01 00:00:00', User_Updated='2023-04-18 00:00:00')
 
-# methods that need to added to LL design
+    def test_delete_user(self):
+        username = "user1"
 
-# 1. some sort of filter for the drop down roles tab, that will filter users by TA or Instructor
-# 6. method that logs user out if they click on logout button
-# 7. method that deletes user account if they click the delete icon button
+        User.deleteUser(username)
+
+        self.assertFalse(User.objects.filter(User_LogName="user1").exists(), "User exists but they were deleted")
 
 
-# these will all just be embedded within html (my brain is thinking we're using Javascript but we're not)
-# 2. method that takes user to the create account page if they click create new account
-# 3. method that takes user to the user's current account page if they click on the hyperlinked user name
-# 4. method that takes user to the user's current account page, in edit mode if they click the edit icon button
-# 5. method that takes user to homepage if they click on home button
+# tests all the navigation functionality on the page
+class TestButtons(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user1 = User.objects.create(User_ID='1', User_Name='Cricket', User_Email='user1@example.com',
+                                         User_Type='student', User_Phone='1234567890', User_Address='123 Main St',
+                                         User_LogName='user1', User_LogPass='password', User_isGrader='no',
+                                         User_begin='2022-01-01 00:00:00', User_Updated='2023-04-18 00:00:00')
+
+    # click create new account button
+    def test_create_new_account_view(self):
+        response = self.client.get('/createaccount/')
+        self.assertEqual(response.status_code, 200)
+
+    # click on the hyperlinked username
+    def test_user_account_view(self):
+        response = self.client.get(f'/account/{self.user1.User_Name}')
+        self.assertEqual(response.status_code, 200)
+
+    # click on the edit icon button
+    def test_edit_account_view(self):
+        response = self.client.get(f'/editaccount/{self.user1.User_Name}')
+        self.assertEqual(response.status_code, 200)
+
+    # click on the home button
+    def test_go_home_view(self):
+        response = self.client.get('/index/')
+        self.assertEqual(response.status_code, 200)
