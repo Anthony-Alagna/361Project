8087c23418a35b91ff7e9b80fa6a395c0711eddc
kmaule15 <kaylamaule15@gmail.com>
2023-04-19 21:20:24 -0500

    added Home to imports

--- TASchedulerApp/urls.py
+++ TASchedulerApp/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import AccountBase, Login
+from myapp.views import AccountBase, Login, Home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
