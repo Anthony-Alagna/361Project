1a6ea52d825c358cf8928a8a06efe820157f101e
aburmai2 <aburmai2@uwm.edu>
2023-04-20 11:44:37 -0500

    editing for webpage

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from myapp.models import User, Course,Section
+
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Course)
--- tests/test_assigning_to_course.py
+++ tests/test_assigning_to_course.py
@@ -55,20 +55,20 @@ class TestInstructorsInCourse(TestCase):
                                              Course_Instructor=self.user1, Course_isOnline='False',
                                              Course_Location='123 Main St',
                                              User_begin='2022-01-01 00:00:00', User_Updated='2023-04-18 00:00:00')
-        self.course1 = Course.objects.create(Course_ID='2', Course_Name='Computer ', Course_Code='482-01',
-                                             Course_Instructor=self.user1, Course_isOnline='False',
+        self.course2 = Course.objects.create(Course_ID='2', Course_Name='Computer Architecture', Course_Code='482-01',
+                                             Course_Instructor=self.user2, Course_isOnline='False',
                                              Course_Location='123 Main St',
                                              User_begin='2022-01-01 00:00:00', User_Updated='2023-04-18 00:00:00')
         self.course3 = Course.objects.create(
             Course_ID='3', Course_Name='Python for Beginners', Course_Code='101-01',
-            Course_Instructor=self.user2, Course_isOnline='True',
+            Course_Instructor="", Course_isOnline='True',
             Course_Location='Online',
             User_begin='2022-01-01 00:00:00', User_Updated='2023-04-18 00:00:00'
         )
 
     def test_user_teaches_course(self):
         teacher = "user1"
-        instructor = Course.getInstructor(self.course1)
+        instructor = User.getInstructor(self.course1)
         self.assertEqual(teacher, instructor)
 
     def test_user_teaches_course(self):
@@ -78,15 +78,47 @@ class TestInstructorsInCourse(TestCase):
 
 # test the instructor  a class with no instructor, and instructor with
 # test location when isOnline
-
-
-class TestAssignUser(TestCase):
+class AddInstructorsToCourse(TestCase):
     def setUp(self):
         self.user1 = User.objects.create(id='1', User_Name='Cricket', User_Email='user1@example.com',
-                                         User_Type='TA', User_Phone='1234567890', User_Address='123 Main St',
+                                         User_Type='instructor', User_Phone='1234567890', User_Address='123 Main St',
                                          User_LogName='user1', User_LogPass='password', User_isGrader='no',
                                          User_begin='2022-01-01 00:00:00', User_Updated='2023-04-18 00:00:00')
-        self.user2 = User.objects.create(id='2', User_Name='Noodle', User_Email='user2@example.com',
-                                         User_Type='TA', User_Phone='0987654321', User_Address='456 Elm St',
-                                         User_LogName='user2', User_LogPass='password1', User_isGrader='no',
+        self.user2 = User.objects.create(id='2', User_Name='taco', User_Email='user2@example.com',
+                                         User_Type='Insturctor', User_Phone='0987654321', User_Address='456 Elm St',
+                                         User_LogName='user2', User_LogPass='password2', User_isGrader='no',
                                          User_begin='2022-01-01 00:00:00', User_Updated='2023-04-17 00:00:00')
+        self.user3 = User.objects.create(id='3', User_Name='Noodle', User_Email='user3@example.com',
+                                         User_Type='TA', User_Phone='034567921', User_Address='22 blanco Dr',
+                                         User_LogName='user2', User_LogPass='password3', User_isGrader='no',
+                                         User_begin='2022-01-01 00:00:00', User_Updated='2023-04-17 00:00:00')
+        self.course1 = Course.objects.create(Course_ID='1', Course_Name='Lion King analysis', Course_Code='382-01',
+                                             Course_Instructor=self.user1, Course_isOnline='False',
+                                             Course_Location='123 Main St',
+                                             User_begin='2022-01-01 00:00:00', User_Updated='2023-04-18 00:00:00')
+        self.course2 = Course.objects.create(Course_ID='2', Course_Name='Computer Architecture', Course_Code='482-01',
+                                             Course_Instructor=self.user2, Course_isOnline='False',
+                                             Course_Location='123 Main St',
+                                             User_begin='2022-01-01 00:00:00', User_Updated='2023-04-18 00:00:00')
+        self.course3 = Course.objects.create(
+            Course_ID='3', Course_Name='Python for Beginners', Course_Code='101-01',
+            Course_Instructor="", Course_isOnline='True',
+            Course_Location='Online',
+            User_begin='2022-01-01 00:00:00', User_Updated='2023-04-18 00:00:00'
+        )
+
+    def add_teacher_to_class_with_no_instructor(self):
+        teacher = self.user1
+        User.assignInstructors(self.course3.Course_Code, self.user1)
+        course = Course.objects.get(Course_ID='2')
+        self.assertIn(teacher, course.Course_Instructor, "teacher should be added because it was blank")
+
+    def add_ta_to_instructor_position(self):
+        User.assignInstructors(self.course3.Course_Code, self.user3)
+        course = Course.objects.get(Course_ID='3')
+        self.assertEqual("", course.Course_Instructor,
+                         "teacher should be added because a TA was inapporpriatly added to the Instructor position"
+
+    def
+
+
