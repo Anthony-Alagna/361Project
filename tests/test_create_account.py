434a2105773f73a2d5000a4df8702772fad5f924
kmaule15 <kaylamaule15@gmail.com>
2023-04-22 16:07:17 -0500

    changed User to Supervisor in create account test file

--- tests/test_create_account.py
+++ tests/test_create_account.py
@@ -14,67 +14,67 @@ class CreateAccountTest(TestCase):
 
     # adds user to the database correctly
     def test_create_account_successful(self):
-        result = User.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
+        result = Supervisor.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
                                     "Milwaukee", "4144165289", "Instructor")
         self.assertEqual(result, User.objects.filter(username="mscott"), "Created account not found in database")
 
     # doesn't add user to the database if username already exists
     def test_create_account_username_exists(self):
-        result = User.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
+        result = Supervisor.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
                                     "Milwaukee", "4144165289", "Instructor")
         self.assertNotEqual(result, User.objects.filter(username="mscott"), "Account should not have been created, username already exists")
 
     # doesn't add user to the database if one of the form field parameters is missing - will redirect back to page with information still filled in
 
     def test_create_account_field_fname_missing(self):
-        result = User.createAccount("Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
+        result = Supervisor.createAccount("Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
                                 "Milwaukee", "4144165289", "Instructor")
         self.assertNotEqual(result, User.objects.filter(User_LogName="mscott"),
                      "Account should not have been created, first name missing")
 
     def test_create_account_field_lname_missing(self):
-        result = User.createAccount("Michael", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
+        result = Supervisor.createAccount("Michael", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
                                     "Milwaukee", "4144165289", "Instructor")
         self.assertNotEqual(result, User.objects.filter(User_LogName="mscott"),
                          "Account should not have been created, last name missing")
 
     def test_create_account_field_email_missing(self):
-        result = User.createAccount("Michael", "Scott", "mscott", "password", "123 Oakland Ave",
+        result = Supervisor.createAccount("Michael", "Scott", "mscott", "password", "123 Oakland Ave",
                                     "Milwaukee", "4144165289", "Instructor")
         self.assertNotEqual(result, User.objects.filter(User_LogName="mscott"),
                          "Account should not have been created, email missing")
     def test_create_account_field_username_missing(self):
-        result = User.createAccount("Michael", "Scott", "mscott@uwm.edu", "password", "123 Oakland Ave",
+        result = Supervisor.createAccount("Michael", "Scott", "mscott@uwm.edu", "password", "123 Oakland Ave",
                                     "Milwaukee", "4144165289", "Instructor")
         self.assertNotEqual(result, User.objects.filter(User_LogPass="password"),
                          "Account should not have been created, username missing")
 
     def test_create_account_field_password_missing(self):
-        result = User.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "123 Oakland Ave",
+        result = Supervisor.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "123 Oakland Ave",
                                     "Milwaukee", "4144165289", "Instructor")
         self.assertNotEqual(result, User.objects.filter(User_LogName="mscott"),
                          "Account should not have been created, password missing")
 
     def test_create_account_field_address_missing(self):
-        result = User.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password",
+        result = Supervisor.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password",
                                     "Milwaukee", "4144165289", "Instructor")
         self.assertNotEqual(result, User.objects.filter(User_LogName="mscott"),
                          "Account should not have been created, address missing")
 
     def test_create_account_field_city_missing(self):
-        result = User.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
+        result = Supervisor.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
                                     "4144165289", "Instructor")
         self.assertNotEqual(result, User.objects.filter(User_LogName="mscott"),
                          "Account should not have been created, city missing")
 
     def test_create_account_field_number_missing(self):
-        result = User.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
+        result = Supervisor.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
                                     "Milwaukee", "Instructor")
         self.assertNotEqual(result, User.objects.filter(User_LogName="mscott"),
                          "Account should not have been created, number missing")
 
     def test_create_account_field_position_missing(self):
-        result = User.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
+        result = Supervisor.createAccount("Michael", "Scott", "mscott@uwm.edu", "mscott", "password", "123 Oakland Ave",
                                     "Milwaukee", "4144165289")
         self.assertNotEqual(result, User.objects.filter(User_LogName="mscott"),
                          "Account should not have been created, position missing")
