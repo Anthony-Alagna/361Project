949a8a6cfb9ff902de88bffbed7f89316272d609
AnthonyAlagna <alalagna@uwm.edu>
2023-04-23 23:37:27 -0500

    fix test_get_account_info_unknown_account

--- tests/test_account_personal_information.py
+++ tests/test_account_personal_information.py
@@ -21,18 +21,12 @@ class PersonalInformationTest(unittest.TestCase):
     def test_get_account_info(self):
         resp = Users.getAccountInfo(
             self.user1.User_LogName, self.user1.id)
-        self.assertEqual(resp[0], "tester", "First name should be Bob")
-        self.assertEqual(resp[1], "Smith", "Last name should be Smith")
-        self.assertEqual(resp[2], "user1ish",
-                         "Usernamelogname should be user1")
-        self.assertEqual(
-            resp[3], "user1@example.com")
-        self.assertEqual(resp[4], "TA", "Position should be TA")
+        self.assertEqual(resp, self.user1, "Should return the user object")
 
     def test_get_account_info_unknown_account(self):
-        # Should raise an exception if the account doesn't exist
-        with self.assertRaises(LookupError, "Should raise exception if account doesn't exist"):
-            users.getAccountInfo("fakeusername")
+        # should return DoesNotExist error if user doesn't exist
+        self.assertRaises(User.DoesNotExist,
+                          Users.getAccountInfo, "unknown", 1)
 
     def test_get_account_info_no_email(self):
         self.assertEqual("", users.getAccountInfo(self.user2.username), "Should return an empty string if email"
