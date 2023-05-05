3f9e63d14e465f53ae4ff9844fbc411f64c8e421
AnthonyAlagna <alalagna@uwm.edu>
2023-05-05 00:08:41 -0500

    first implementation of login and tests

--- myapp/Classes/login.py
+++ myapp/Classes/login.py
@@ -1,3 +1,5 @@
+from random import random
+import string
 from myapp.models import User, Course, Section, CourseToUser
 import smtplib
 
@@ -22,13 +24,37 @@ class ForgotPassword:
     """
 
     def send_reset_email(self, username):
+
+        # Generate a token for the user and store it in the database
+        user = User.objects.get(username=username)
+        token = self._generate_token(username)
+        user.pw_reset_token = token
+        user.save()
+
+        # Send the email to the user
         try:
             server = smtplib.SMTP('smtp.gmail.com', 587)
             server.starttls()
             server.login(self.email, self.password)
-            message = f"Subject: Password reset for {username}\n\nPlease click on the following link to reset your password: http://example.com/reset/{username}"
+            message = self._generate_message(token)
             server.sendmail(self.email, username, message)
             server.quit()
             print("Password reset email sent successfully!")
         except:
             print("Sorry, something went wrong. Please try again later.")
+
+        """
+        _summary_ : generates a token for the user to reset their password
+        _params_ : username - The username of the account that needs to reset their password
+        """
+
+    def _generate_token(self, username):
+        # Generate a random string of 20 characters for the authentication portion of the token
+        auth_str = ''.join(random.choices(
+            string.ascii_uppercase + string.ascii_lowercase + string.digits, k=20))
+        # Combine the username and authentication string to create the token
+        token = f"{username}:{auth_str}"
+        return token
+
+    def _generate_message(token):
+        message = f"Click the following link to reset your password: "
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,5 +1,8 @@
 from django.test import TestCase, Client
+from django.urls import reverse
 from myapp.models import User
+from myapp.Classes import login
+import os
 
 """_summary_ contains unit tests for the login page
     """
@@ -13,6 +16,10 @@ class LoginTest(TestCase):
                 email=(i),
                 User_LogPass=str(i),
             )
+
+    def tearDown(self):
+        for i in range(10):
+            User.objects.get(email=(i)).delete()
 
     def test_login_accessible(self):
         """_summary_ tests that the login page is accessible"""
@@ -51,3 +58,37 @@ class LoginTest(TestCase):
     def test_login_post_no_username_and_invalid_password(self):
         response = self.client.post("", {"username": "", "password": "2"})
         self.assertEqual(response.status_code, 302)
+
+# TODO implement this test
+
+
+class ForgotPasswordTest(TestCase):
+    def setUp(self):
+        self.fp = login.ForgotPassword(
+            os.getenv("MAIL_USERNAME"), os.getenv("MAIL_PASSWORD"))
+        self.client = Client()
+        User.objects.create(
+            email=os.getenv("MAIL_USERNAME"),
+            User_LogPass=str(1),
+        )
+
+    def test_forgot_password_accessible(self):
+        """_summary_ tests that the forgot password page is accessible"""
+        response = self.client.get(reverse("forgotpassword"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_forgot_password_post(self):
+        """_summary_ tests that the forgot password form can be submitted"""
+        response = self.client.post("forgotpassword/", {"username": "1"})
+        self.assertContains(response, "Password reset email sent.")
+
+    def test_forgot_password_post_invalid_username(self):
+        """_summary_ tests that an error message is displayed for invalid usernames"""
+        response = self.client.post(
+            "forgotpassword/", {"username": "invalid"})
+        self.assertContains(response, "Invalid username or email address.")
+
+    def test_forgot_password_post_no_username(self):
+        """_summary_ tests that an error message is displayed for missing usernames"""
+        response = self.client.post("forgotpassword/", {})
+        self.assertContains(response, "This field is required.")
