eb705450cede63c6966383a7d3ccba918b36e45e
AnthonyAlagna <alalagna@uwm.edu>
2023-05-07 00:22:39 -0500

    convert login to use django provided mail client

--- myapp/Classes/login.py
+++ myapp/Classes/login.py
@@ -1,9 +1,7 @@
 import random
-import ssl
 import string
 from myapp.models import User, Course, Section, CourseToUser
-import smtplib
-import os
+from django.core.mail import send_mail
 
 # this class implements methods to send the user
 # an email to reset their password if they forgot it
@@ -23,35 +21,35 @@ class ForgotPassword:
 
     """_summary_ : sends the user an email to reset their password
         _params_ : username - The username of the account that needs to reset their password
+                     message - The message to be sent to the user (optional)
         _returns_ : True if the email was sent successfully, otherwise raises an error
     """
 
     def send_reset_email(self, username, message=None):
-        # create a secure ssl context
-        context = ssl.create_default_context()
 
         if message is None:
             token = self._generate_token(username)
             # Generate the message to be sent to the user if one was not provided
             message = self._generate_message(
-                "Password Reset", f"Please click the link below to reset your password:\n\n{token}")
+                "Password Reset", f"Please click the link below to reset your password:\n\n{token}"
+            )
 
-        # Send the email to the user
         try:
-            server = smtplib.SMTP(os.getenv("MAIL_SERVER"), 587)
-            server.starttls(context=context)
-            server.login(self.email, self.password)
-            server.sendmail(self.email, username, message)
+            send_mail(
+                "Password Reset",  # subject
+                message,  # message
+                self.email,  # from_email
+                [username],  # recipient_list
+                fail_silently=False,
+            )
             return True
         except Exception as e:
-            print(e)
-        finally:
-            server.quit()
+            raise e
 
-        """
-        _summary_ : generates a token for the user to reset their password
-        _params_ : username - The username of the account that needs to reset their password
-        """
+    """
+    _summary_ : generates a token for the user to reset their password
+    _params_ : username - The username of the account that needs to reset their password
+    """
 
     def _generate_token(self, username):
         # Generate a random string of 20 characters for the authentication portion of the token
