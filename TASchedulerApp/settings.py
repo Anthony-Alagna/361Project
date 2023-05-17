f11ea67e9d1e2f8fa311b4138df2d2c9090c3e6d
kmaule15 <kaylamaule15@gmail.com>
2023-05-17 08:32:56 -0500

    CSS now works again

--- TASchedulerApp/settings.py
+++ TASchedulerApp/settings.py
@@ -139,7 +139,12 @@ if env == "development":
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
