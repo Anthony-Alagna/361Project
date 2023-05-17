8b664b84c81f3454d65c39ea6b3c5fe80c909067
kmaule15 <kaylamaule15@gmail.com>
2023-05-17 08:36:32 -0500

    Update settings.py

--- TASchedulerApp/settings.py
+++ TASchedulerApp/settings.py
@@ -138,11 +138,11 @@ if env == "development":
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+
+STATIC_URL = "static/"
 
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
