2c5f75771ed0c02ee33e98e263d1b8cc74fe1f21
kmaule15 <93933029+kmaule15@users.noreply.github.com>
2023-05-18 21:07:57 -0500

    Update test_edit_course.py

--- tests/Acceptance Tests/test_edit_course.py
+++ tests/Acceptance Tests/test_edit_course.py
@@ -1,16 +1,15 @@
-from django.test import TestCase, Client
-from django.urls import reverse
+# from django.test import TestCase, Client
+# from django.urls import reverse
 
-from myapp.models import Course, User
-from myapp.Classes.supervisor import Supervisor
+# from myapp.models import Course, User
+# from myapp.Classes.supervisor import Supervisor
 
 
-class TestButtons(TestCase):
-#     BRING BACK IN
-#     def test_edit_course_page_accessible(self):
-#         response = self.client.get(reverse("courseedit"))
-#         self.assertEqual(
-#             response.status_code,
-#             200,
-#             "Expected status_code 200, got " + str(response.status_code),
-#         )
+# # class TestButtons(TestCase):
+# #     def test_edit_course_page_accessible(self):
+# #         response = self.client.get(reverse("courseedit"))
+# #         self.assertEqual(
+# #             response.status_code,
+# #             200,
+# #             "Expected status_code 200, got " + str(response.status_code),
+# #         )
